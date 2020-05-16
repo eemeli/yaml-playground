e895ee4d5bc24709c6e394ba6643ac91bb73994c
Eemeli Aro <eemeli@gmail.com>
2020-05-16 14:40:12 +0300

    Adjust for default-true prettyErrors

--- src/index.js
+++ src/index.js
@@ -20,13 +20,7 @@ function getJSON(src) {
     doc.errors.length > 0
       ? doc.errors
           .slice(0, 5)
-          .map(error => {
-            const pos = error.source.rangeAsLinePos
-            let head = error.name
-            if (pos && pos.start)
-              head += ` at line ${pos.start.line}, column ${pos.start.col}`
-            return `${head}:\n  ${error.message}`
-          })
+          .map(error => `${error.name}:\n  ${error.message}`)
           .join('\n')
       : JSON.stringify(doc.toJSON(), null, '  ')
   )
